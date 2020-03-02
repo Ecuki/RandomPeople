9ad975d0201282395db3f3513cb38541d38b4788
Ecuki <jeziorski.emil@gmail.com>
2020-03-02 22:23:46 +0100

    Updates

---
+++ precache-manifest.28cdc53d1d475e0c2ebe9fdd8f1e9ce0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a105ee44f6e4df02a639752fec25866",
+    "url": "/RandomPeople/index.html"
+  },
+  {
+    "revision": "6c5904e37320d555cc7d",
+    "url": "/RandomPeople/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "6f018d138e426b097cab",
+    "url": "/RandomPeople/static/css/main.b242eade.chunk.css"
+  },
+  {
+    "revision": "6c5904e37320d555cc7d",
+    "url": "/RandomPeople/static/js/2.b49454ee.chunk.js"
+  },
+  {
+    "revision": "8c563181c00d3b074e703b1af571b61c",
+    "url": "/RandomPeople/static/js/2.b49454ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f018d138e426b097cab",
+    "url": "/RandomPeople/static/js/main.8d30a794.chunk.js"
+  },
+  {
+    "revision": "50465ed4938751931dcc",
+    "url": "/RandomPeople/static/js/runtime-main.fbcd0af3.js"
+  },
+  {
+    "revision": "b199aefd7e994ad15b79bcbc5efb76ce",
+    "url": "/RandomPeople/static/media/logo2.b199aefd.png"
+  }
+]);
